c3229cc809b9f5ff50ee430cec2f3355f0438650
jeffleyg <jeffleygarcon007@gmail.com>
2026-03-13 23:14:55 -0300

    Implement models/client.ts

--- betalent-api/app/models/client.ts
+++ betalent-api/app/models/client.ts
@@ -1,4 +1,23 @@
-import { ClientSchema } from '#database/schema'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon';
+import Transaction from './transaction';
 
-export default class Client extends ClientSchema {
+export default class Client extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare email: string
+
+  @column.DateTime({autoCreate: true})
+  declare createdAt: DateTime
+
+  @column.DateTime({autoCreate: true, autoUpdate: true})
+  declare updatedAt: DateTime
+
+  @hasMany(() => Transaction)
+  declare transactions: hasMany<typeof Transaction>
 }
