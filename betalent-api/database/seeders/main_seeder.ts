a5f7bd342335e08e4d183b7a452cb5b6f0478c46
jeffleyg <jeffleygarcon007@gmail.com>
2026-03-13 23:43:25 -0300

    feat: add main_seeder.ts

--- betalent-api/database/seeders/main_seeder.ts
+++ betalent-api/database/seeders/main_seeder.ts
@@ -1,6 +1,11 @@
+import Gateway from '#models/gateway'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
+    await Gateway.updateOrCreateMany('name', [
+      { name: 'gateway1', priority: 1, isActive: true },
+      { name: 'gateway2', priority: 2, isActive: true },
+    ])
   }
 }
