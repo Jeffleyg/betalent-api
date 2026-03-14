20f69f0cf0b613e65fb966d86113177c89ede1bc
jeffleyg <jeffleygarcon007@gmail.com>
2026-03-13 23:18:36 -0300

    Implement models/transaction.ts

--- betalent-api/app/models/transaction.ts
+++ betalent-api/app/models/transaction.ts
@@ -3,22 +3,41 @@ import { BaseModel, column, belongsTo, manyToMany } from '@adonisjs/lucid/orm'
 import type { BelongsTo, ManyToMany } from '@adonisjs/lucid/types/relations'
 import Product from '#models/product'
 import Client from '#models/client'
+import Gateway from './gateway'
 
 export default class Transaction extends BaseModel {
-  @column({ isPrimary: true })
+ @column({ isPrimary: true })
   declare id: number
 
   @column()
   declare clientId: number
 
   @column()
-  declare gatewayId: number
+  declare gatewayId: number | null
+
+  @column()
+  declare externalId: string | null
+
+  @column()
+  declare productId: number
+
+  @column()
+  declare quantity: number
 
   @column()
   declare totalAmount: number
 
   @column()
   declare status: 'pending' | 'paid' | 'failed' | 'refunded'
+
+  @column()
+  declare cardLastNumbers: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
 
   @manyToMany(() => Product, {
     pivotTable: 'transaction_products',
@@ -27,4 +46,7 @@ export default class Transaction extends BaseModel {
 
   @belongsTo(() => Client)
   declare client: BelongsTo<typeof Client>
+
+  @belongsTo(() => Gateway)
+  declare gateway: BelongsTo<typeof Gateway>
 }
